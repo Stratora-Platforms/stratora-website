f6ef27d1a7406fc687cc265b1fff6ab4f4131cb7
Nick Manganiello <nick@stratora.io>
2026-05-27 16:26:50 -0400

    chore: release v2.2.1

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 // Stratora release version — update this for each new release
-export const RELEASE_VERSION = "2.2.0";
+export const RELEASE_VERSION = "2.2.1";
 export const MSI_DOWNLOAD_URL = `https://github.com/Stratora-Platforms/stratora-releases/releases/download/v${RELEASE_VERSION}/Stratora-Server-${RELEASE_VERSION}.msi`;
 export const RELEASE_NOTES_URL = `https://github.com/Stratora-Platforms/stratora-releases/releases/tag/v${RELEASE_VERSION}`;
 
